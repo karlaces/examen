710318bd9d3e8fdfd3fb638a894a4ef44e7be729
karlacesgu <97060610+karlacesgu@users.noreply.github.com>
2022-07-28 15:01:23 -0500

    migracion user_domicilio

---
+++ database/migrations/2022_07_28_195543_create_user_domicilio_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserDomicilioTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_domicilio', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('user_id');
+            $table->string('domicilio', 255)->nullable();
+            $table->string('numero_exterior')->nullable();
+            $table->string('colonia', 255)->nullable();
+            $table->string('cp')->nullable();
+            $table->string('ciudad')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_domicilio');
+    }
+}
